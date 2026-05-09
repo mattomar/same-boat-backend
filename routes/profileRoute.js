b49c7b01784bf7ed0bdeabca04bd4ed3c21d152f
mattomar <moddathero@gmail.com>
2026-05-09 19:26:24 +0700

    connected posts to user profile

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -2,22 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 
-const { User, Profile } = require("../models");
-
+const { User, Profile, Post, Category } = require("../models");
 
 
 router.get("/:userId", async (req, res) =>  {
   try {
     const userId = req.params.userId;
 
-    const user = await User.findByPk(userId, {
-      attributes: ["id", "firstName", "lastName", "username", "email"],
-      include: {
-        model: Profile,
-        as: "profile",
-        attributes: ["bio", "avatarUrl", "phone"],
-      },
-    });
+const user = await User.findByPk(userId, {
+  attributes: ["id", "firstName", "lastName", "username", "email"],
+  include: [
+    {
+      model: Profile,
+      as: "profile",
+      attributes: ["bio", "avatarUrl", "phone"],
+    },
+    {
+      model: Post,
+      as: "posts",
+      include: [
+        {
+          model: Category,
+          as: "category",
+          attributes: ["id", "name"],
+        },
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "username"],
+        },
+      ],
+    },
+  ],
+});
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
