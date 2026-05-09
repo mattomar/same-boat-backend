b392cb460d91bf0a51933fe9e5181b8fca1420b6
mattomar <moddathero@gmail.com>
2026-05-09 19:27:49 +0700

    Fix Sequelize associations for user data in posts

--- models/user.js
+++ models/user.js
@@ -1,52 +1,64 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      bio: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      avatarUrl: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        validate: {
-          isValidUrl(value) {
-            if (value && !/^https?:\/\/.+/.test(value)) {
-              throw new Error("Invalid URL format");
-            }
-          },
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    avatarUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isValidUrl(value) {
+          if (value && !/^https?:\/\/.+/.test(value)) {
+            throw new Error("Invalid URL format");
+          }
         },
       },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          len: [3, 25],
-          is: /^[a-zA-Z0-9_.-]*$/i, // optional: only allow letters, numbers, underscore, etc.
-        },
-      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 25],
+        is: /^[a-zA-Z0-9_.-]*$/i,
+      },
+    },
+  });
+
+  User.associate = (models) => {
+    User.hasOne(models.Profile, {
+      foreignKey: "userId",
+      as: "profile",
     });
-  
-    return User;
+
+    User.hasMany(models.Post, {
+      foreignKey: "userId",
+      as: "posts",
+    });
   };
+
+  return User;
+};
